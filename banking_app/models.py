35f588ab3d3e72fe6979d1dacf6bc4f8f13b9c34
Chase Harrison <ChaseH88@gmail.com>
2023-10-19 02:27:27 -0500

    clean up

--- banking_app/models.py
+++ banking_app/models.py
@@ -12,12 +12,19 @@ class Account(models.Model):
     ]
 
     account_number = models.PositiveIntegerField(
-        unique=True, validators=[MaxValueValidator(99999999)])
+        unique=True,
+        validators=[MaxValueValidator(99999999)]
+    )
     account_pin = models.PositiveIntegerField(
-        validators=[MaxValueValidator(9999)])
+        validators=[MaxValueValidator(9999)]
+    )
     account_type = models.CharField(
-        max_length=8, choices=ACCOUNT_TYPES, default=CHECKING)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+        max_length=8, choices=ACCOUNT_TYPES, default=CHECKING
+    )
+    balance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
@@ -37,11 +44,20 @@ class Transaction(models.Model):
 
     transaction_id = models.AutoField(primary_key=True)
     transaction_type = models.CharField(
-        max_length=10, choices=TRANSACTION_TYPES, default=DEPOSIT)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+        max_length=10,
+        choices=TRANSACTION_TYPES,
+        default=DEPOSIT)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
     date = models.DateField()
-    account = models.ForeignKey(Account, on_delete=models.CASCADE,
-                                related_name='transactions', to_field='account_number')
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name='transactions',
+        to_field='account_number'
+    )
 
     def __str__(self):
         return f"{self.transaction_type.capitalize()} - ${self.amount} on {self.date}"
